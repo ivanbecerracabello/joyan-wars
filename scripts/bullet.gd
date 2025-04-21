85405213c031f00e0cf52cf9790ac3f9c255dfb1
Ivan <donivanbecerra@gmail.com>
2025-04-21 19:18:27 +0200

    Third-person controller

---
+++ scripts/bullet.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+const SPEED = 40
+
+@onready var mesh = $MeshInstance3D
+@onready var ray = $RayCast3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.position += transform.basis * Vector3(0, 0, -SPEED) * delta
+	if ray.is_colliding():
+		mesh.visible = false
+		queue_free()
---
+++ scripts/gate.gd
@@ -0,0 +1,22 @@
+extends StaticBody3D
+
+var closed
+var current_rotation
+
+func _ready():
+	closed = true
+	current_rotation = rad_to_deg(rotation.y)
+
+func toggle():
+	var tween = create_tween()
+	
+	if closed:
+		tween.tween_property(self, "rotation:y", deg_to_rad(current_rotation - 60), 3)
+		closed = false
+	else:
+		tween.tween_property(self, "rotation:y", deg_to_rad(current_rotation), 3)
+		closed = true
+
+func _input(event):
+	if event.is_action_pressed("action"):
+		toggle()
